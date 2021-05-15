7404ffe7378b4350f609de99ea5185d30f616f98
blackadder <scope.denis@mail.ru>
2021-05-15 22:19:53 +0300

    Added 1st version of the notebook and __init__.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CLIP_experimental
+---
 Repository for experiments with several approaches to fine-tune model, pretrained on the CLIP:  https://openai.com/blog/clip/
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .clip_wrappers import CLIP_Lite, CLIP_Pro
+from .dataset import CLIPDataset
+from .text_transformer import TextTransformer
+from .tokenizer import SimpleTokenizer
