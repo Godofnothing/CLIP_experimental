3760a77d874f1df89fd7d31c7d94475d1c1539de
blackadder <scope.denis@mail.ru>
2021-05-19 17:35:10 +0300

    epta

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from .clip_wrappers import CLIP_Lite, CLIP_Pro
 from .dataset import CLIPDataset
 from .text_transformer import TextTransformer
 from .tokenizer import SimpleTokenizer
-from .dataset import ClassificationVisualizer
+from .inference import ClassificationVisualizer
